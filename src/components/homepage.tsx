faa3c7f8b81664df5e7e37b93d5043f66ca55ffe
amy.youme <amy.youme@kakaocorp.com>
2021-03-02 16:54:54 +0900

    Homepage 원래 코드 주석

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -38,6 +38,19 @@ const Homepage = ({ posts }: PostsProps) => {
 
   return (
     <Blog posts={posts}></Blog>
+    // <Layout>
+    //   <h1 sx={visuallyHidden}>{siteTitle}</h1>
+    //   <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 }, variant: `section_hero` }}>
+    //     <Hero />
+    //   </section>
+    //   <Title text="Latest Posts">
+    //     <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>Read all posts</Link>
+    //   </Title>
+    //   <Listing posts={posts} showTags={false} />
+    //   <List sx={{ variant: `section_bottom` }}>
+    //     <Bottom />
+    //   </List>
+    // </Layout>
   )
 }
 
